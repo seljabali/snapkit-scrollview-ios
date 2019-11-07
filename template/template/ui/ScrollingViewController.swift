82b4196a853b1b2609645d0e3e3518baa96ca0b8
Sami Eljabali <seljabali@gmail.com>
2019-11-06 18:03:58 -0700

    make content view within scrollview a PassThroughView to allow click pass through

--- template/template/ui/ScrollingViewController.swift
+++ template/template/ui/ScrollingViewController.swift
@@ -5,7 +5,7 @@ import UIKit
 class ScrollingViewController: UIViewController {
 
     private let scrollView = UIScrollView()
-    private let contentView = UIView()
+    private let contentView = PassThroughView()
 
     private let aLabel = UILabel()
     private let bLabel = UILabel()
@@ -24,7 +24,7 @@ class ScrollingViewController: UIViewController {
     }
 
     private func initializeViews() {
-        // Hierarchy: view -> scrollView -> contentView -> labels
+        // Hierarchy: view controller's view -> scrollView -> contentView/passThroughView -> content/labels
         view.addSubview(scrollView)
         scrollView.addSubview(contentView)
         for subview in [aLabel, bLabel, cLabel, dLabel, eLabel] {
@@ -64,7 +64,6 @@ class ScrollingViewController: UIViewController {
             make.edges.equalTo(view)
         }
         contentView.snp.makeConstraints { make in
-            // Make the top and bottom always be the top and bottom of the scroll view (since we want to scroll vertically only)
             make.top.bottom.equalTo(scrollView)
             make.left.right.equalTo(view)
             make.width.equalTo(scrollView)
@@ -84,11 +83,10 @@ class ScrollingViewController: UIViewController {
             make.top.equalTo(cLabel.snp.bottom).offset(VerticalSpacings.m)
             make.left.right.equalTo(contentView).inset(HorizontalSpacings.m)
         }
-        // What you forgot to do is set a constraint for the "bottom" of the (now contentView) to the bottom of the last label (`eLabel`). You must do this, otherwise, there is no rule telling it to make sure that the bottom of the final label must always line up with the bottom of the scrollview!
         eLabel.snp.makeConstraints { make in
             make.top.equalTo(dLabel.snp.bottom).offset(VerticalSpacings.m)
             make.left.right.equalTo(contentView).inset(HorizontalSpacings.m)
-            // So, add this rule. Remember, you're saying "make sure that the bottom of the contentView is lined up with the bottom of the last label" or "make sure the bottom of the last label is the bottom of the contentView" (which is the same thing, right?) -- note that I added an inset here
+            // Make sure that the bottom of the contentView is lined up with the bottom of the last view
             make.bottom.equalTo(scrollView).offset(VerticalSpacings.m)
         }
     }
---
+++ template/template/utilities/PassThroughView.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+public class PassThroughView: UIView {
+
+    override public func point(inside point: CGPoint, with event: UIEvent?) -> Bool {
+        for subview in subviews {
+            if !subview.isHidden && subview.isUserInteractionEnabled && subview.point(inside: convert(point, to: subview), with: event) {
+                return true
+            }
+        }
+        return false
+    }
+
+}
